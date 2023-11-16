6d6f18a2cfcd1fe44a5dfeea70b66f9e2a7627a7
John Dodis <jfdodis@yahoo.com>
2023-11-16 05:15:08 +0200

    cleanup conf and vscode

--- conf.lua
+++ conf.lua
@@ -5,3 +5,10 @@ function love.conf( t )
 	t.console = true
 	t.modules.joystick = false
 end
+
+-- function lovr.conf( t )
+-- 	t.headset = false
+-- 	t.window.resizable = true
+-- 	t.window.width = 1300
+-- 	t.window.height = 600
+-- end
